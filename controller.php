630f9f29b0bef7c956e2d30438dcba1ad46396a3
Jimut <jemdhali67@gmail.com>
2016-03-31 22:20:12 +0530

    Final Update

--- controller.php
+++ controller.php
@@ -1,26 +1,19 @@
 <?php
-  error_reporting(E_ALL);
-  ini_set("display_errors", 1);
 
-  // do {
   $tempfile = tempnam('', 'source');
-  // }while(!@rename($tempfile, $tempfile.'.c'));
   @rename($tempfile, $tempfile.'.c');
 
   $tempfile .= '.c';
   $exefile = 'test.o';
 
-  echo $tempfile;
-
   file_put_contents($tempfile, $_REQUEST['c_code']);
 
   // invoke GCC
   $output = shell_exec('gcc '.escapeshellarg($tempfile).' -o '.escapeshellarg($exefile));
-  // set sticky bit
-  $output.= shell_exec('sudo +s '.escapeshellarg($exefile)); // I need to set this on my server
+
   // run the created program
   $output .= shell_exec(escapeshellarg($exefile));
 
   echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';
 
-  // unlink($tempfile);
+  unlink($tempfile);
