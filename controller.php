203f477be8ca87074c639c8062f779a89a7438d1
Jimut <jemdhali67@gmail.com>
2016-03-31 20:19:05 +0530

    Initial Commit

---
+++ controller.php
@@ -0,0 +1,22 @@
+<?php
+
+  // do {
+  $tempfile = tempnam('C:/xampp/htdocs/jem/code-compiler/temp', 'source');
+  // }while(!@rename($tempfile, $tempfile.'.c'));
+  @rename($tempfile, $tempfile.'.c');
+
+  $tempfile .= '.c';
+  $exefile = 'test.exe';
+
+  file_put_contents($tempfile, $_REQUEST['c_code']);
+
+  // invoke GCC
+  $output = shell_exec('gcc '.escapeshellarg($tempfile).' -o '.escapeshellarg($exefile));
+  // set sticky bit
+  // $output.= shell_exec('sudo +s '.escapeshellarg($exefile)); // I need to set this on my server
+  // run the created program
+  $output .= shell_exec(escapeshellarg($exefile));
+
+  echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';
+
+  unlink($tempfile);
