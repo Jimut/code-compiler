a5014fd383aced4f655fa55e47df99c188fa904a
Jimut <jemdhali67@gmail.com>
2016-09-16 21:22:51 +0530

    Error and stdin input features added

--- controller.php
+++ controller.php
@@ -1,19 +1,47 @@
 <?php
 
-  $tempfile = tempnam('', 'source');
-  @rename($tempfile, $tempfile.'.c');
+$codefile = tempnam(__DIR__.'/temp', 'source');
+@rename($codefile, $codefile.'.c');
 
-  $tempfile .= '.c';
-  $exefile = 'test.o';
+$codefile .= '.c';
+$exefile = 'prog.exe';
+$errorfile = 'error-output.txt';
 
-  file_put_contents($tempfile, $_REQUEST['c_code']);
+file_put_contents($codefile, $_REQUEST['c_code']);
 
-  // invoke GCC
-  $output = shell_exec('gcc '.escapeshellarg($tempfile).' -o '.escapeshellarg($exefile));
+$output_gcc = shell_exec('gcc '.escapeshellarg($codefile).' -o '.escapeshellarg('temp/'.$exefile).'2>&1');
 
-  // run the created program
-  $output .= shell_exec(escapeshellarg($exefile));
+if ($output_gcc !== NULL) {
 
-  echo '<pre>'.htmlspecialchars($output,ENT_QUOTES).'</pre>';
+  echo '<pre>'.htmlspecialchars($output_gcc, ENT_QUOTES).'</pre>';
 
-  unlink($tempfile);
+} else {
+
+  $stdin = $_REQUEST['stdin'];
+
+  $descriptorspec = [
+    ['pipe', 'r'],
+    ['pipe', 'w'],
+    ['file', 'temp/'.$errorfile, 'a']
+  ];
+
+  $process = proc_open($exefile, $descriptorspec, $pipes, __DIR__.'/temp');
+
+  if (is_resource($process)) {
+    fwrite ($pipes[0], $stdin);
+    fclose ($pipes[0]);
+
+    echo stream_get_contents($pipes[1]);
+    fclose ($pipes[1]);
+
+    $return_value = proc_close($process);
+
+    if (!$return_value) echo file_get_contents('temp/'.$errorfile);
+  }
+  
+  unlink('temp/'.$exefile);
+  unlink('temp/'.$errorfile);
+}
+
+unlink($codefile);
+
--- index.php
+++ index.php
@@ -7,7 +7,11 @@
   <body>
 
     <form action="controller.php" method="POST">
+      <label for="c_code">Code</label>
       <textarea name="c_code" id="c_code" cols="30" rows="10"></textarea>
+
+      <label for="stdin">Stdin</label>
+      <textarea name="stdin" id="stdin" cols="30" rows="5"></textarea>
       <button type="submit">Submit</button>
     </form>
 
